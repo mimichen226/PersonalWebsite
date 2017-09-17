4cfeb95226c21617e68a457382371ef835100cd6
Mimi Chen <mchen6@nd.edu>
2017-09-17 03:34:22 -0400

    Made About Me secion responsive

--- js/application.js
+++ js/application.js
@@ -1,5 +1,4 @@
 function changeBitmoji(){
-    // Get current bitmoji img
     var bitmoji=document.getElementById("bitmoji");
     path = bitmoji.src.split('/');
     image = path[path.length - 1];
@@ -7,7 +6,5 @@ function changeBitmoji(){
     image_id = (image_id + 1) % 4;
     image = "images/Bitmoji/bitmoji-" + image_id.toString() + ".png";
     bitmoji.src = image
-
-    // Set to new bitomji
 }
-window.setInterval(changeBitmoji, 3000);
+window.setInterval(changeBitmoji, 5000);
