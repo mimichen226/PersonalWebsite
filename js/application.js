51466b7aa192c03999a5a54111a000161433656a
Mimi Chen <mchen6@nd.edu>
2017-09-17 12:38:29 -0400

    Finished description for projects

--- js/application.js
+++ js/application.js
@@ -4,7 +4,7 @@ function changeBitmoji(){
     image = path[path.length - 1];
     image_id = parseInt(image.split('-')[1].split('.')[0]);
     image_id = (image_id + 1) % 4;
-    image = "images/Bitmoji/bitmoji-" + image_id.toString() + ".png";
+    image = "images/bitmoji/bitmoji-" + image_id.toString() + ".png";
     bitmoji.src = image
 }
 window.setInterval(changeBitmoji, 2000);
